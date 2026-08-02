f7b71f65aa52ef4b26ad2d6754b682f69e2bbb62
jahid <jahidhasanbsl46@gmail.com>
2026-08-02 16:43:37 +0600

    nextjs + django first commit

---
+++ store/views.py
@@ -0,0 +1,19 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+
+
+@api_view(["GET"])
+def get_products(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get_categories(request):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories, many=True)
+    return Response(serializer.data)
